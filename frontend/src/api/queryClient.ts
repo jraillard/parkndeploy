ef0bd727ee6071d19b4976ed09897f98e49feb2f
jraillard <dangrall49@gmail.com>
2024-10-10 14:48:52 +0200

    setup gc & staletime in react query

--- frontend/src/api/queryClient.ts
+++ frontend/src/api/queryClient.ts
@@ -5,7 +5,8 @@ export const queryClient = new QueryClient({
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
-      staleTime: 30000,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
     }
   }
 });
