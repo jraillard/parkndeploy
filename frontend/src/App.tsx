37c1546e1c468debbea0367673f282310082033c
jraillard <dangrall49@gmail.com>
2025-08-14 11:34:18 +0200

    feat(frontend) : display current package.json version
    + css updates to make it appear well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,24 +13,32 @@ function App() {
 
   const { data, isPending, isError } = useQuery({
     queryKey: [PARKINGS_QUERY_KEY, { parkingName }],
-    queryFn: () => ParkingsAngersEndpointsQueryMethods.getAllParkings(parkingName),
+    queryFn: () =>
+      ParkingsAngersEndpointsQueryMethods.getAllParkings(parkingName),
   });
 
-  console.log(parkingName)
-
   return (
-    <div className="flex flex-col gap-5 items-center">
-      <h1 className="text-2xl font-bold text-center">
-        Where can I Park in Angers ? 👀
-      </h1>
-      <ParkingListFilters
-        onChange={(parkingName: string) => {
-          console.log(parkingName);
-        }}
-      />
-      {isPending && <LoadingSpinner className="mr-2 h-4 w-4 animate-spin" />}
-      {isError && <span>Something went wrong with the backend ...</span>}
-      {data && <ParkingList parkings={data.parkings} />}
+    <div className="flex flex-col gap-4 items-center">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold text-center">
+          Where can I Park in Angers ? 👀
+        </h1>
+
+        <ParkingListFilters
+          onChange={(parkingName: string) => {
+            console.log(parkingName);
+          }}
+        />
+      </div>
+      <div className="h-screen">
+        {isPending && <LoadingSpinner className="mr-2 h-4 w-4 animate-spin" />}
+        {isError && <span>Something went wrong with the backend ...</span>}
+        {data && <ParkingList parkings={data.parkings} />}
+      </div>
+
+      <div className="fixed bottom-0 left-0 right-0 z-40 px-4 py-3 text-center text-white bg-gray-800">
+        ParkNDeploy {APP_VERSION}
+      </div>
     </div>
   );
 }
