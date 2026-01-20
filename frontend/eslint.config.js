ef8a5c49c987b2233d1d2db4c3236224f522aadf
Khalilelk20 <khalilkarci@gmail.com>
2026-01-20 17:53:52 +0100

    Test modif pull

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+// Test modif CI PR workflow
 
 export default tseslint.config(
   { ignores: ['dist'] },
