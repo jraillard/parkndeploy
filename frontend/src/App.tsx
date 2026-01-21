76ef9ebb1b29706d6957900024d68efe6dc9b831
usrlocal <usrlocal@GA213-FE06.ad.univ-angers.fr>
2026-01-21 13:09:45 +0100

    feat(frontend) : added version static text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,9 @@ function App() {
       <h1 className="text-2xl font-bold text-center">
         Where can I Park in Angers ? 👀
       </h1>
+      <h2 className="text-center">
+        v1.0.0
+      </h2>
       <ParkingListFilters
         onChange={(parkingName: string) => {
           console.log(parkingName);
