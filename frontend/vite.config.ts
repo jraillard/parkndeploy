8fd424f6be911b0f975093fdc4e63294900a5d68
Oumaima741 <oumaima.kaadade@uit.ac.ma>
2026-01-21 12:16:01 +0100

    done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import pkg from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:
-    {
-      // redirect request on /api to target specified
-      '/api': {        
-        target: 'https://localhost:7085/', // whether you're running your local api with kestrel or behind IIS Express, the port mind change
-        secure: false, // Do not verify SSL certificates
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7085/",
+        secure: false,
       },
-    }
-  },
-  resolve: {    
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
     },
-  },  
+  },
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+  },
   plugins: [react()],
   define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    APP_VERSION: JSON.stringify(process.env.VITE_APP_VERSION ?? pkg.version),
   },
-})
+});
