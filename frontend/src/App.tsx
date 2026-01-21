4ee6c1c583d2acd8c9b9893b2aa8b3ff734719d9
dorian-hode <dorianhode@gmail.com>
2026-01-21 16:51:09 +0100

    Test with version in the frontend 3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="flex flex-col gap-5 items-center">
       <h1 className="text-2xl font-bold text-center">
-        Where can I Park in Angers ? 👀  {APP_VERSION}
+        Where can I Park in Angers ? 👀
       </h1>
       <ParkingListFilters
         onChange={(parkingName: string) => {
@@ -31,6 +31,7 @@ function App() {
       {isPending && <LoadingSpinner className="mr-2 h-4 w-4 animate-spin" />}
       {isError && <span>Something went wrong with the backend ...</span>}
       {data && <ParkingList parkings={data.parkings} />}
+      <p>App Version: {APP_VERSION}</p>
     </div>
   );
 }
